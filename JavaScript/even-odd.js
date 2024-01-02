03e1345e0ee48c92481909da40c752a493fcbe9a
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-02 21:18:18 +0530

    even or odd number find

--- JavaScript/even-odd.js
+++ JavaScript/even-odd.js
@@ -1,2 +1,14 @@
 // Check If Number is Even Or Odd 
 
+let isEven = (num) => {
+    if (num % 2 == 0) {
+        console.log(`${num} is Even Number`);
+    }
+    else {
+        console.log(`${num} is Odd Number`);
+    }
+}
+isEven(5);
+isEven(6);
+isEven(7);
+isEven(9);
