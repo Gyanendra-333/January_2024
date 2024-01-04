09f207fe3c8ef3df2c4d3897f17dd4ae2ea46cf6
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-04 16:53:02 +0530

    star

--- JavaScript/pyramid.js
+++ JavaScript/pyramid.js
@@ -6,7 +6,10 @@ let str;
 for (let i = 1; i <= rows; i++) {
     str = "";
 
-    for (let j = 1; j <= columns; j++) {
+    for (let j = 1; j <= (rows - i); j++) {
+        str += " ";
+    }
+    for (let k = 0; k != (2 * i - 1); k++) {
         str += "*";
     }
     console.log(str);
