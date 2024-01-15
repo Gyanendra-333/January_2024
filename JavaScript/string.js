07b1cdb193fcc8b2ba1a1d32d64becfc488b4866
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-15 23:45:50 +0530

    str

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -5,3 +5,7 @@ console.log("String 1: " + str1);
 // Creating a String using String() Constructor
 const str2 = String("Second String Content");
 console.log("String 2: " + str2);
+
+// Creating a String using String() Constructor
+const str3 = String("Third String Content");
+console.log("String 2: " + str2 + str3);
