cc652c1da9e12c56fd1302b84b8aee767e156d30
Gyanendra Prakash <pgyanendra333@gmail.com>
2024-01-15 23:44:40 +0530

    String

---
+++ JavaScript/string.js
@@ -0,0 +1,7 @@
+// Create a variable and assign String value
+const str1 = "First String Content";
+console.log("String 1: " + str1);
+
+// Creating a String using String() Constructor
+const str2 = String("Second String Content");
+console.log("String 2: " + str2);
